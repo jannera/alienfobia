9114cd89e5777ef116cf56906f6b20afa6a0a7f7
gilead <janne.rasanen@tut.fi>
2014-10-16 18:56:11 +0300

    Initial commit

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+    public GameObject playerPrefab;
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    private const string roomName = "RoomName";
+    private RoomInfo[] roomsList;
+
+    void OnGUI()
+    {
+        if (!PhotonNetwork.connected)
+        {
+            GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        }
+        else if (PhotonNetwork.room == null)
+        {
+            // Create Room
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+                PhotonNetwork.CreateRoom(roomName + System.Guid.NewGuid().ToString("N"), true, true, 5);
+ 
+            // Join Room
+            if (roomsList != null)
+            {
+                for (int i = 0; i < roomsList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].name))
+                        PhotonNetwork.JoinRoom(roomsList[i].name);
+                }
+            }
+        }
+    }
+
+    void OnReceivedRoomListUpdate()
+    {
+        roomsList = PhotonNetwork.GetRoomList();
+    }
+    void OnJoinedRoom()
+    {
+        Debug.Log("Connected to Room");
+        PhotonNetwork.Instantiate(playerPrefab.name, Vector3.up * 5, Quaternion.identity, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : Photon.MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    void Update()
+    {
+        if (photonView.isMine)
+        {
+            InputMovement();
+            InputColorChange();
+        }
+        else
+        {
+            SyncedMovement();
+        }
+    }
+
+    private void InputColorChange()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            ChangeColorTo(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+    }
+
+    [RPC]
+    void ChangeColorTo(Vector3 color)
+    {
+        renderer.material.color = new Color(color.x, color.y, color.z, 1f);
+
+        if (photonView.isMine)
+            photonView.RPC("ChangeColorTo", PhotonTargets.OthersBuffered, color);
+    }
+
+    private void SyncedMovement()
+    {
+        syncTime += Time.deltaTime;
+        rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+    }
+
+    void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+            rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+    }
+
+    private float lastSynchronizationTime = 0f;
+    private float syncDelay = 0f;
+    private float syncTime = 0f;
+    private Vector3 syncStartPosition = Vector3.zero;
+    private Vector3 syncEndPosition = Vector3.zero;
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(rigidbody.position);
+            stream.SendNext(rigidbody.velocity);
+        }
+        else
+        {
+            Vector3 syncPosition = (Vector3)stream.ReceiveNext();
+            Vector3 syncVelocity = (Vector3)stream.ReceiveNext();
+
+            syncTime = 0f;
+            syncDelay = Time.time - lastSynchronizationTime;
+            lastSynchronizationTime = Time.time;
+
+            syncEndPosition = syncPosition + syncVelocity * syncDelay;
+            syncStartPosition = rigidbody.position;
+        }
+    }
+}
