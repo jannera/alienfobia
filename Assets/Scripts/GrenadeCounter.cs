87e4bf5dc0f9855730e36c76a452c464404e6a85
jannera <janne.rasanen@vincit.fi>
2014-10-19 02:03:34 +0300

    Added grenade counter

---
+++ Assets/Scripts/GrenadeCounter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class GrenadeCounter : MonoBehaviour
+    {
+        private Text text;
+        // Use this for initialization
+        void Start()
+        {
+            text = GetComponent<Text>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            GameObject playerGun = GameObject.FindGameObjectWithTag("PlayerGun");
+            if (playerGun == null)
+            {
+                text.text = "";
+                return;
+            }
+            PlayerShooting playerShooting = playerGun.GetComponent<PlayerShooting>();
+
+            text.text = "X " + playerShooting.grenades;
+        }
+    }
+
+}
