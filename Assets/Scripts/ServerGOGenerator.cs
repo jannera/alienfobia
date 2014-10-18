9e33082a6f321bf0c1d3801c4487ecb850aa16fa
Eero Kirjavainen <eero.kirjavainen@vincit.fi>
2014-10-18 21:04:14 +0300

    Monster generator parameters

--- Assets/Scripts/ServerGOGenerator.cs
+++ Assets/Scripts/ServerGOGenerator.cs
@@ -2,9 +2,15 @@
 using System.Collections;
 
 public class ServerGOGenerator : MonoBehaviour
-{
-		public float chancePerFrame = 0.05f;
-		public GameObject toBeGenerated;
+{		
+		public GameObject toBeGenerated;		
+		public int spawnRange = 1;
+		public int secBetweenWaves = 5;
+		public int spawnedPerWave = 10;
+		public int maxwaves = 10;
+		
+		private float currentTime = 0.0f;
+		private int spawnedWaves = 0;
 
 		// Use this for initialization
 		void Start ()
@@ -24,14 +30,34 @@ public class ServerGOGenerator : MonoBehaviour
 						return;
 				}
 
-				if (Random.Range (0, 1f) < chancePerFrame) {
-						GenerateGameObject ();
+				currentTime += Time.deltaTime;
+				Debug.Log("Current time " + currentTime) ;
+				
+				if (currentTime >= secBetweenWaves) {
+					GenerateGameObjects(spawnedPerWave);	
+					currentTime -= (int)currentTime;								
 				}
 		}
-
-		private void GenerateGameObject ()
+	
+		private void GenerateGameObjects (int count)
 		{
+			if (spawnedWaves <= maxwaves) {
+				Debug.Log("Spawning wave " + spawnedWaves) ;
 				object[] p = { PhotonNetwork.player.ID };
-				PhotonNetwork.InstantiateSceneObject (toBeGenerated.name, transform.position, Quaternion.identity, 0, p);
+				for (int i = 0; i <= count; ++i) {
+					SpawnWithinRange(p);
+				}
+				++spawnedWaves;	
+			} else {
+				Debug.Log("No more spawns");
+			}
+		}
+
+		private void SpawnWithinRange(object[] playerId) {
+			Vector3 spawnLocation = new Vector3(0f, 0f, 0f);			
+			spawnLocation.x = transform.position.x + Random.Range(-spawnRange, spawnRange);
+			spawnLocation.z = transform.position.z + Random.Range(-spawnRange, spawnRange);
+			
+			PhotonNetwork.InstantiateSceneObject (toBeGenerated.name, spawnLocation, Quaternion.identity, 0, playerId);							
 		}
 }
