6a44bd4c93e0644a4b8c177a1b60b86210f43a9b
Antti Palola <antti.palola@vincit.fi>
2014-10-18 17:59:22 +0300

    Fixes
    * Remove prefabs
    * spawn bunnies instead of balls

--- Assets/Scripts/ServerGOGenerator.cs
+++ Assets/Scripts/ServerGOGenerator.cs
@@ -1,37 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ServerGOGenerator : MonoBehaviour {
-    public float chancePerFrame = 0.05f;
-    public GameObject toBeGenerated;
+public class ServerGOGenerator : MonoBehaviour
+{
+		public float chancePerFrame = 0.05f;
+		public GameObject toBeGenerated;
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start ()
+		{
         
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
-        if (!PhotonNetwork.inRoom)
-        {
-            return;
-        }
+		// Update is called once per frame
+		void Update ()
+		{
+				if (!PhotonNetwork.inRoom) {
+						return;
+				}
         
-        if (!PhotonNetwork.isMasterClient)
-        {
-            Destroy(this);
-            return;
-        }
+				if (!PhotonNetwork.isMasterClient) {
+						Destroy (this);
+						return;
+				}
 
-        if (Random.Range(0, 1f) < chancePerFrame)
-        {
-            GenerateGameObject();
-        }
-	}
+				if (Random.Range (0, 1f) < chancePerFrame) {
+						GenerateGameObject ();
+				}
+		}
 
-    private void GenerateGameObject()
-    {
-        object[] p = { PhotonNetwork.player.ID };
-        PhotonNetwork.InstantiateSceneObject(toBeGenerated.name, Vector3.up * 5, Quaternion.identity, 0, p);
-    }
+		private void GenerateGameObject ()
+		{
+				object[] p = { PhotonNetwork.player.ID };
+				PhotonNetwork.InstantiateSceneObject (toBeGenerated.name, transform.position, Quaternion.identity, 0, p);
+		}
 }
