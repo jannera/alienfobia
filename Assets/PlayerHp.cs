e2fc56c6e6ecbc0bcaed523d7a9b6450915f7f46
Antti Palola <antti.palola@vincit.fi>
2014-10-19 01:14:33 +0300

    Fix player HUD
    * completely new player hp script that listens to player hp changes and
      reacts to it

---
+++ Assets/PlayerHp.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+	public class PlayerHp : MonoBehaviour {
+	
+		public UnityEngine.UI.Slider slider;
+		public UnityEngine.UI.Image hitImage;
+		int lastHealth;
+
+		public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
+		public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
+	
+		PlayerHealth playerHealth;
+		
+		// Update is called once per frame
+		void Update () {
+			if (playerHealth == null) {
+				GameObject player = GameObject.FindWithTag ("Player");
+				if (player != null) {
+					playerHealth = player.GetComponent<PlayerHealth>();
+				}
+				else {
+					return;
+				}
+
+			}
+
+			slider.value = playerHealth.currentHealth;
+
+			// If the player has just been damaged...
+			if(lastHealth != playerHealth.currentHealth)
+			{
+				// ... set the colour of the damageImage to the flash colour.
+				hitImage.color = flashColour;
+			}
+			// Otherwise...
+			else
+			{
+				hitImage.color = Color.Lerp (hitImage.color, Color.clear, flashSpeed * Time.deltaTime);
+			}
+			
+			// Reset the damaged flag.
+			lastHealth = playerHealth.currentHealth;
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -8,11 +8,9 @@ namespace CompleteProject
     {
         public int startingHealth = 100;                            // The amount of health the player starts the game with.
         public int currentHealth;                                   // The current health the player has.
-        public Slider healthSlider;                                 // Reference to the UI's health bar.
-        public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
+
         public AudioClip deathClip;                                 // The audio clip to play when the player dies.
-        public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
-        public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
+        
 
 
         Animator anim;                                              // Reference to the Animator component.
@@ -38,20 +36,6 @@ namespace CompleteProject
 
         void Update ()
         {
-            // If the player has just been damaged...
-            if(damaged)
-            {
-                // ... set the colour of the damageImage to the flash colour.
-                //damageImage.color = flashColour;
-            }
-            // Otherwise...
-            else
-            {
-                //damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-            }
-
-            // Reset the damaged flag.
-            damaged = false;
         }
 
 
@@ -62,9 +46,6 @@ namespace CompleteProject
 
             // Reduce the current health by the damage amount.
             currentHealth -= amount;
-
-            // Set the health bar's value to the current health.
-            healthSlider.value = currentHealth;
 
             // Play the hurt sound effect.
             playerAudio.Play ();
