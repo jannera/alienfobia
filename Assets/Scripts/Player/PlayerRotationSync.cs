60711f60bc59c7214a06bf5665fb1ef8510d5d2a
gilead <janne.rasanen@tut.fi>
2014-10-22 22:03:51 +0300

    Bear rotations are synced

--- Assets/Scripts/Player/PlayerRotationSync.cs
+++ Assets/Scripts/Player/PlayerRotationSync.cs
@@ -12,43 +12,11 @@ namespace CompleteProject
      * and everyone else listens. Everyone also makes sure that when 
      * someone elses Players rotation changes, the change happens smoothly.
      * */
-    class PlayerRotationSync : CompleteProject.PhotonBehaviour
+    class PlayerRotationSync : CompleteProject.RotationSync
     {
-        private Quaternion syncRotation;
-        public float smoothTurning = 30;
-
-        private GameObject player;
-
         void Awake()
         {
-            player = PlayerManager.GetPlayerWithOwnerId(photonView.owner.ID);
-        }
-
-        void Update()
-        {
-            if (photonView.isMine)
-            {
-                return; // no estimates on your own player
-            }
-
-            // Debug.Log("estimating " + ownerId);
-            // everyone estimates on other players, even the master client
-            player.transform.rotation = Quaternion.Slerp(player.transform.rotation, syncRotation, Time.deltaTime * smoothTurning);
-        }
-
-        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-        {
-
-            if (stream.isWriting)
-            {
-                // writing is only called for owners of the object
-                stream.SendNext(player.transform.rotation);
-            }
-            else
-            {
-                // reading is only called for non-owners of the object
-                syncRotation = (Quaternion)stream.ReceiveNext();
-            }
+            syncedGO = PlayerManager.GetPlayerWithOwnerId(photonView.owner.ID);
         }
     }
 }
