0f05584bdfd6e61c94f24589109756a97d84a4ee
jannera <janne.rasanen@vincit.fi>
2014-10-24 17:19:38 +0300

    Notify other players of gun effects via RPCs

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -7,13 +7,13 @@ namespace CompleteProject
         public int damagePerShot = 20;                  // The damage inflicted by each bullet.
         public float timeBetweenBullets = 0.15f;        // The time between each shot.
         public float timeBetweenRockets = 2f;
-		public float timeToReload = 1f;
+        public float timeToReload = 1f;
         public float range = 100f;                      // The distance the gun can fire.
 
 
         float timer;                                    // A timer to determine when to fire.
         float rocketTimer = 0;
-		float reloadTimer = 0;
+        float reloadTimer = 0;
         Ray shootRay;                                   // A ray from the gun end forwards.
         RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
         int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
@@ -23,52 +23,73 @@ namespace CompleteProject
         AudioSource gunReload;
         Light gunLight;                                 // Reference to the light component.
         float effectsDisplayTime = 0.2f;                // The proportion of the timeBetweenBullets that the effects will display for.
-		public const int clipSize = 60;
-		public int bullets = clipSize;
+        public const int clipSize = 60;
+        public int bullets = clipSize;
 
         public GameObject grenadePreFab;
 
         public int grenades = 3;
 
         private bool isMine;
+        private bool effectsDisplayedOnce = false;
 
 
-        void Awake ()
+        void Awake()
         {
             // Create a layer mask for the Shootable layer.
-            shootableMask = LayerMask.GetMask ("Shootable");
+            shootableMask = LayerMask.GetMask("Shootable");
 
             // Set up the references.
-            gunParticles = GetComponent<ParticleSystem> ();
-            gunLine = GetComponent <LineRenderer> ();
-            gunAudio = GetComponents<AudioSource> () [0];
+            gunParticles = GetComponent<ParticleSystem>();
+            gunLine = GetComponent<LineRenderer>();
+            gunAudio = GetComponents<AudioSource>()[0];
             gunReload = GetComponents<AudioSource>()[1];
-            gunLight = GetComponent<Light> ();
+            gunLight = GetComponent<Light>();
 
             isMine = gameObject.GetComponentInParent<PositionSync>().isMine;
         }
 
 
-        void Update ()
+        void Update()
         {
+            // EXECUTED FOR EVERYONE
+            // Add the time since Update was last called to the timer.
+            timer += Time.deltaTime;
+
+            // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
+            if (timer >= timeBetweenBullets * effectsDisplayTime)
+            {
+                // ... disable the effects.
+                if (effectsDisplayedOnce)
+                {
+                    DisableEffects();
+                }
+                else
+                {
+                    effectsDisplayedOnce = true;
+                }
+            }
+
             if (!isMine)
             {
                 return;
             }
-            // Add the time since Update was last called to the timer.
-            timer += Time.deltaTime;
-            rocketTimer += Time.deltaTime;
-			reloadTimer += Time.deltaTime;
 
-			if (reloadTimer >= timeToReload && bullets == 0) {
-				bullets = clipSize;
-			}
+            // EXECUTED FOR ONLY THE OWNING PLAYER
+            
+            rocketTimer += Time.deltaTime;
+            reloadTimer += Time.deltaTime;
+
+            if (reloadTimer >= timeToReload && bullets == 0)
+            {
+                bullets = clipSize;
+            }
 
             // If the Fire1 button is being press and it's time to fire...
-            if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets)
+            if (Input.GetButton("Fire1") && timer >= timeBetweenBullets)
             {
                 // ... shoot the gun.
-                Shoot ();
+                Shoot();
             }
 
             if (Input.GetButton("Fire2") && rocketTimer >= timeBetweenRockets && grenades > 0)
@@ -77,14 +98,6 @@ namespace CompleteProject
                 ThrowGrenade();
             }
 
-            // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
-            if(timer >= timeBetweenBullets * effectsDisplayTime)
-            {
-                // ... disable the effects.
-                DisableEffects ();
-            }
-
-            
             if (Input.GetKey(KeyCode.R))
             {
                 StartReloading();
@@ -92,7 +105,7 @@ namespace CompleteProject
         }
 
 
-        public void DisableEffects ()
+        public void DisableEffects()
         {
             // Disable the line renderer and the light.
             gunLine.enabled = false;
@@ -106,59 +119,75 @@ namespace CompleteProject
             gunReload.Play();
         }
 
-        void Shoot ()
+        void Shoot()
         {
-			if (bullets == 0) {
+            if (bullets == 0)
+            {
                 return;
-			}
-			--bullets;
-			if (bullets == 0) {
+            }
+            --bullets;
+            if (bullets == 0)
+            {
                 StartReloading();
                 return;
-			}
+            }
             // Reset the timer.
             timer = 0f;
-
-            // Play the gun shot audioclip.
-            gunAudio.Play ();
-
-            // Enable the light.
-            gunLight.enabled = true;
-
-            // Stop the particles from playing if they were, then start the particles.
-            gunParticles.Stop ();
-            gunParticles.Play ();
-
-            // Enable the line renderer and set it's first position to be the end of the gun.
-            gunLine.enabled = true;
-            gunLine.SetPosition (0, transform.position);
 
             // Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
             shootRay.origin = transform.position;
             shootRay.direction = transform.forward;
 
+            Vector3 firingEndPos;
             // Perform the raycast against gameobjects on the shootable layer and if it hits something...
-            if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
             {
                 // Try and find an EnemyHealth script on the gameobject hit.
-                EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+                EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
 
                 // If the EnemyHealth component exist...
-                if(enemyHealth != null)
+                if (enemyHealth != null)
                 {
                     // ... the enemy should take damage.
-                    enemyHealth.TakeDamage (damagePerShot, shootHit.point);
+                    enemyHealth.TakeDamage(damagePerShot, shootHit.point);
                 }
 
                 // Set the second position of the line renderer to the point the raycast hit.
-                gunLine.SetPosition (1, shootHit.point);
+                firingEndPos = shootHit.point;
+                
             }
             // If the raycast didn't hit anything on the shootable layer...
             else
             {
                 // ... set the second position of the line renderer to the fullest extent of the gun's range.
-                gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+                firingEndPos = shootRay.origin + shootRay.direction * range;
             }
+
+            // TODO: actually, because ray positions must be calculated locally on each client, we just need to
+            // pass the enemy and the local hit point on the enemy to each client
+            RPC<Vector3>(EnableFiringEffects, PhotonTargets.All, firingEndPos);
+        }
+
+        [RPC]
+        private void EnableFiringEffects(Vector3 firingEndPos)
+        {
+            timer = 0f;
+
+            // Play the gun shot audioclip.
+            gunAudio.Play();
+
+            // Enable the light.
+            gunLight.enabled = true;
+
+            // Stop the particles from playing if they were, then start the particles.
+            gunParticles.Stop();
+            gunParticles.Play();
+
+            // Enable the line renderer and set it's first position to be the end of the gun.
+            gunLine.enabled = true;
+            gunLine.SetPosition(0, transform.position);
+            gunLine.SetPosition(1, firingEndPos);
+            effectsDisplayedOnce = false;
         }
 
         void ThrowGrenade()
@@ -187,4 +216,4 @@ namespace CompleteProject
             return Mathf.Clamp(reloadTimer / timeToReload, 0, 1);
         }
     }
-}	
+}
