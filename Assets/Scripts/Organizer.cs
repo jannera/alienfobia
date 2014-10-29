ab33ac72fafe98e058aa11834029f0d403913404
gilead <janne.rasanen@tut.fi>
2014-10-29 23:49:14 +0200

    Less clutter in the hierarchy
    Reorganized all level specific stuff under one folder
    Added automatic organizing of players, enemies and pickups under folders in hierarchy.

---
+++ Assets/Scripts/Organizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    // organizes gameobjects under given gameobject automatically
+    public class Organizer : MonoBehaviour
+    {
+        public GameObject group;
+
+        void Start()
+        {
+            GameObject parent = GameObject.Find(group.name);
+            if (parent == null)
+            {
+                parent = (GameObject) Instantiate(group, Vector3.zero, Quaternion.identity);
+                parent.name = group.name;
+            }
+            transform.parent = parent.transform;
+        }
+    }
+}
