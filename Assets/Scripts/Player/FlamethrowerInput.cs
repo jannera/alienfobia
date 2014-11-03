e88e24f924d35bef1b947f9d75ea8727a58d7a17
gilead <janne.rasanen@tut.fi>
2014-11-03 22:42:36 +0200

    Flamethrower now uses particles also for collision detection, and has sound

---
+++ Assets/Scripts/Player/FlamethrowerInput.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlamethrowerInput : MonoBehaviour {
+
+    private ParticleSystem particles;
+    private AudioSource audio;
+    private const float fadeOutTime = 0.5f;
+    private float fader;
+
+	// Use this for initialization
+	void Awake() {
+        particles = GetComponent<ParticleSystem>();
+        audio = GetComponent<AudioSource>();
+
+        particles.Stop();
+        audio.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+        {
+            particles.Play();
+            audio.volume = 1f;
+            fader = fadeOutTime;
+
+            if (!audio.isPlaying)
+            {
+                audio.Play();    
+                audio.time = 0.5f;
+            }
+        }
+        else
+        {
+            fader -= Time.deltaTime;
+            if (fader < 0)
+            {
+                fader = 0;
+                audio.Stop();
+            }
+            particles.Stop();
+            audio.volume = fader / fadeOutTime;
+        }
+	}
+}
---
+++ Assets/Scripts/Player/FlamethrowerParticleDamager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CompleteProject
+{
+    public class FlamethrowerParticleDamager : MonoBehaviour
+    {
+
+        public int damagePerUpdate = 10;
+
+        private List<GameObject> itemsInFlame = new List<GameObject>(10);
+
+        void OnParticleCollision(GameObject go)
+        {
+            if (!go.CompareTag("Enemy"))
+            {
+                return;
+            }
+            if (itemsInFlame.Contains(go))
+            {
+                return;
+            }
+            itemsInFlame.Add(go);
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            foreach (GameObject go in itemsInFlame)
+            {
+                EnemyHealth enemyHealth = go.GetComponent<EnemyHealth>();
+
+                if (enemyHealth != null)
+                {
+                    enemyHealth.TakeDamage(damagePerUpdate, go.transform.position);
+                }   
+            }
+            itemsInFlame.Clear();
+        }
+
+        void OnEnable()
+        {
+            itemsInFlame.Clear();
+        }
+    }
+}
