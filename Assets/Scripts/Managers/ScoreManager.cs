96163600c9aad3aaf5d5c75b165895549d992f41
gilead <janne.rasanen@iki.fi>
2014-11-22 16:49:40 +0200

    Only show score, no kills

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -6,24 +6,18 @@ namespace CompleteProject
 {
     public class ScoreManager : MonoBehaviour
     {
-        public static int score;        // The player's score.
-        public static int kills;        // The player's total kills
-        public Text killsText;
+        public static int score;
+        public static int kills;
         public Text scoreText;
-
 
         void Awake()
         {
-            // Reset the score.
             score = 0;
             kills = 0;
         }
 
-
         void Update()
         {
-            // Set the displayed text to be the word "Score" followed by the score value.
-            killsText.text = "Kills: " + kills;
             scoreText.text = "Score: " + score;
         }
     }
