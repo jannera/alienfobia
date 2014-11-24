dd084a1ce8286965e9d866a8c1dfb4f6519820cd
gilead <janne.rasanen@iki.fi>
2014-11-24 22:20:40 +0200

    Added first version of countdown clock

---
+++ Assets/Scripts/UI/CountdownClock.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class CountdownClock : MonoBehaviour {
+    public Animator levelCountdownAnimator;
+
+    private Text text;
+
+    float stopTime = float.NaN;
+
+	void Start () {
+        text = GetComponent<Text>();
+        // todo: once level ending signal is given, freeze the clock
+	}
+	
+	void Update () {
+        if (float.IsNaN(stopTime))
+        {
+            AnimationClip clip = GetCurrentClip();
+            if (clip == null)
+            {
+                return;
+            }
+            stopTime = AnimationUtility.GetAnimationEvents(clip)[0].time;
+        }
+        AnimatorStateInfo info = levelCountdownAnimator.GetCurrentAnimatorStateInfo(0);
+        int secondsLeft = (int) (stopTime - info.length * info.normalizedTime);
+        if (secondsLeft < 0)
+        {
+            secondsLeft = 0;
+        }
+        System.TimeSpan span = System.TimeSpan.FromSeconds(secondsLeft);
+        text.text = span.Minutes.ToString("00") + ":" + span.Seconds.ToString("00");
+	}
+
+    private AnimationClip GetCurrentClip()
+    {
+        AnimationInfo[] infos = levelCountdownAnimator.GetCurrentAnimationClipState(0);
+        if (infos == null)
+        {
+            return null;
+        }
+        if (infos.Length == 0)
+        {
+            return null;
+        }
+        return infos[0].clip;
+    }
+}
