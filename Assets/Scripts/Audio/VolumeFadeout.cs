d1d0e3f32821bfd3b98e43b1869dfa206338f010
gilead <janne.rasanen@iki.fi>
2014-12-03 00:04:05 +0200

    Music fades out if you die

---
+++ Assets/Scripts/Audio/VolumeFadeout.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class VolumeFadeout : MonoBehaviour
+    {
+        public float fadeOutTime = 1f;
+        public AudioSource audioSource;
+        
+        private float startVolume;
+        float timeLasted = 0;
+        void Start()
+        {
+            startVolume = audioSource.volume;
+        }
+
+        void Update()
+        {
+            timeLasted += Time.deltaTime;
+
+            float volume = (1f - timeLasted / fadeOutTime) * startVolume;
+            audioSource.volume = Mathf.Clamp(volume, 0, startVolume);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Player/EnableWhenPlayerDies.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class EnableWhenPlayerDies : MonoBehaviour
+    {
+        public Behaviour target;
+        
+        void Start()
+        {
+            GameState.OnMyPlayerDied += delegate()
+            {
+                target.enabled = true;
+            };
+        }
+
+    }
+}
